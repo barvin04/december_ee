f020007a762180948e8bde9e7ee6497e88bdd0f9
barvin04 <abhinababala04@gmail.com>
2017-12-15 12:04:51 +0530

    improved

--- pyt.py
+++ pyt.py
@@ -11,49 +11,62 @@ keys = g.readlines()
 
 for i in range(len(keys)):
 	keys[i] = keys[i].strip('\n')
-	#print keys[i]
+
 for i in range(len(dict_word)): 
 	dict_word[i] = dict_word[i].split(" ")[0]
-	#print dict_word[i]
+	
 
+wrt = open("write_50.txt", "w") #write format 
+wrtn = open("write_50_names.txt", "w")
 for i in range(len(dict_word)): 
 	if (dict_word[i] in keys):
-		pass#print dict_vec[j]
+		st =dict_vec[i]
+		wrt.write(st)
+		wrt.write('\n')
+		wrtn.write(dict_word[i])
+		wrtn.write('\n')
+wrt.close()
+wrtn.close()
 
+#for compound names 
+wrt = open("write_50.txt", "a") #append format 
+wrtn = open("write_50_names.txt", "a")
 for j in range(len(keys)): 
 	cntf = 0 
 	cnts = 0	
 	if("+" in keys[j]): 
 		t = keys[j].split("+")
-		f = t[0] 
-		s = t[1]
-		for i in range(len(dict_word)): 
+		f = t[0]  #get the first word
+		s = t[1]	#get the second word
+		for i in range(len(dict_word)):  	#if first word found 
 			if (dict_word[i] == f): 
 				cntf = 1
 				v1 =dict_vec[i]
-				#print v1
-		for i in range(len(dict_word)): 
+				
+		for i in range(len(dict_word)):  	#if second word found 
 			if (dict_word[i] == s) :
 				cnts = 1
 				v2 = dict_vec[i]
-				#print v2 
-		
 
-		v1 = v1.split(" ")[1:][:-1]
+		v1 = v1.split(" ")[1:][:-1]		#using the numercals only 
 		v2 = v2.split(" ")[1:][:-1]
 		
-		#print v1[0], v2[0]
 		for i in range(len(v1)):
 			v1[i] = (float(v1[i]) + float(v2[i]))/2 
 			v1[i] = str(v1[i])
 		temp = [keys[j]]
 		
 		v1 = temp + v1 + ['\n']
-		#print v1[1]
 		
 		#" ".join(v1)     <----- due to some reasons, this is not working 
 		st = ""
 		for elem in v1:
-			st = st+elem+" "
-		print st.strip(" ")
+			st = st+elem+" " #averaged vector created 
+		wrt.write(st)
+		wrt.write('\n')
 		
+		name= st.split(" ")[0]	
+		print name
+		wrtn.write(dict_word[i])  #names in a file being stored 
+		wrtn.write('\n')
+wrt.close()
